00c25c8aaf7d821a7e32d135f23f4dffa8ac0bd2
Kacper Smoczyński <smok123780@gmail.com>
2020-08-09 19:07:18 +0200

    Initial commit

---
+++ android/app/src/main/java/com/kacper/smoczynski/todo_list_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kacper.smoczynski.todo_list_app;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
